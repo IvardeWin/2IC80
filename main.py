4a5f0957fb24557de007666219c9ed4799d0452c
daanb <daanboelhouwers@gmail.com>
2022-06-02 14:34:56 +0200

    Fix type in main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src import dns_spoof as dns
 if __name__ == '__main__':
     net.test()
     input("Press [enter] to start DNS spoofing")
-    dns_spoofing = dns.DNSSpoofing(interfaces=[], domain_names=[], victims=[], redirect_IP="")
+    dns_spoofing = dns.DNSSpoofing(interface=[], domain_names=[], victims=[], redirect_IP="")
     dns_spoofing.start()
     input("Press [enter] to stop DNS spoofing")
     dns_spoofing.stop()
