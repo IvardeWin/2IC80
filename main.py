79d0d652c1d0338124316ee2070d99399e137482
kasteelvuur <kasteelvuur@gmail.com>
2022-06-02 15:25:44 +0200

    Made discover a class

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
+from threading import Thread
+from time import sleep
+
+from scapy.arch import get_if_list
+
 from src import discover
 
+config = dict()
+config["hosts"] = dict()
+
+
 if __name__ == "__main__":
-    discover.test()
+    for interface in get_if_list():
+        config["hosts"][interface] = dict()
+        discover.Discover(config, interface).start()
+    while True:
+        print(config["hosts"])
+        sleep(10)
--- src/discover.py
+++ src/discover.py
@@ -3,46 +3,82 @@ from scapy.layers.inet import IP
 from scapy.layers.l2 import ARP, Ether
 
 
-def local_host_discovered(mac: str, ip: str):
-    """
-    TODO Does something when a host is discovered
+class Discover(threading.Thread):
 
-        :param mac: The mac address of the discovered host
-        :param ip: The ip address of the discovered host
-    """
-    print(f"Found host at mac {mac} and ip {ip}!")
-    # TODO do smth
+    def __init__(self, config: dict, interface: str):
+        super(Discover, self).__init__()
+        self.config = config
+        self.interface = interface
 
+    def stop(self):
+        self._stop.set()
 
-def filter_host(mac: str, ip: str):
-    """
-    Filters out all unwanted hosts
+    def is_stopped(self):
+        return self._stop.isSet()
 
-    Args:
-        :param mac: The mac address of a host 
-        :param ip: The ip address of a host
-    """
-    if mac == "ff:ff:ff:ff:ff:ff":
-        return # Filter out broadcasts
-    # TODO filter out self
-    # TODO filter out router?
-    # TODO filter out non-local?
-    local_host_discovered(mac, ip)
+    def run(self):
+        self.passive()
 
+    def local_host_discovered(self, mac: str, ip: str):
+        """
+        Add found host to the dictionary
 
-def packet_sniffed(pkt: packet):
-    """
-    Retrieves the mac- and ip-address of hosts from sniffed packets
-
-        :param pkt: The sniffed packet
-    """
-    if IP in pkt:
-        filter_host(pkt[Ether].src, pkt[IP].src)
-        filter_host(pkt[Ether].dst, pkt[IP].dst)
-    if ARP in pkt:
-        filter_host(pkt[Ether].src, pkt[ARP].psrc)
-        filter_host(pkt[Ether].dst, pkt[ARP].pdst)
+            :param mac: The mac address of the discovered host
+            :param ip: The ip address of the discovered host
+        """
+        hosts: dict = self.config["hosts"][self.interface]
+        if mac not in hosts:
+            hosts.update(dict({mac: {ip}}))
+        elif ip not in hosts[mac]:
+            hosts[mac].add(ip)
 
 
-def test():
-    sniff(count=100, store=0, prn=packet_sniffed)
+    def filter_host(self, mac: str, ip: str):
+        """
+        Filters out all unwanted hosts
+
+        Args:
+            :param mac: The mac address of a host 
+            :param ip: The ip address of a host
+        """
+        if mac == "ff:ff:ff:ff:ff:ff":
+            return # Filter out broadcasts
+        # TODO filter out self
+        # TODO filter out router?
+        # TODO filter out non-local?
+        self.local_host_discovered(mac, ip)
+
+
+    def packet_sniffed(self, pkt: packet):
+        """
+        Retrieves the mac- and ip-address of hosts from sniffed packets
+
+            :param pkt: The sniffed packet
+        """
+        if IP in pkt:
+            self.filter_host(pkt[Ether].src, pkt[IP].src)
+            self.filter_host(pkt[Ether].dst, pkt[IP].dst)
+        if ARP in pkt:
+            self.filter_host(pkt[Ether].src, pkt[ARP].psrc)
+            self.filter_host(pkt[Ether].dst, pkt[ARP].pdst)
+
+
+    def passive(self):
+        """
+        Passively sniffs packets on the current interface
+        """
+        try:
+            #sniff(iface=self.interface, count=0, store=0, prn=self.packet_sniffed)
+            sniff(count=0, store=0, prn=self.packet_sniffed)
+        except OSError:
+            print(f"Could not open the adapter for interface {self.interface}")
+            self.stop()
+        except Scapy_Exception:
+            print(f"Could not operate on interface {self.interface}")
+            self.stop()
+        except IndexError:
+            print(f"Layer not found")
+            self.stop()
+
+    def active(self):
+        print("Active discovery has not been implemented yet!")
