4e179c0e467020a67c4e841d6aac28f5376a8b2c
daanb <daanboelhouwers@gmail.com>
2022-06-06 16:50:57 +0200

    Set parameters for dns spoofing in main for testing

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src import dns_spoof as dns
 if __name__ == '__main__':
     net.test()
     input("Press [enter] to start DNS spoofing")
-    dns_spoofing = dns.DNSSpoofing(interface=[], domain_names=[], victims=[], redirect_ip="")
+    dns_spoofing = dns.DNSSpoofing(interface="enp0s3", domain_names=["tue.nl"], victims=[], redirect_ip="")
     dns_spoofing.start()
     input("Press [enter] to stop DNS spoofing")
     dns_spoofing.stop()
