ad2aeec0f60c84c5121263fbc052b76234563258
daanb <daanboelhouwers@gmail.com>
2022-06-17 11:19:51 +0200

    Change srp to sendp and add interface to it

--- src/discover_active.py
+++ src/discover_active.py
@@ -1,13 +1,13 @@
 from scapy.arch import get_if_addr
 from scapy.error import Scapy_Exception
 from scapy.layers.l2 import ARP, Ether
-from scapy.sendrecv import srp
+from scapy.sendrecv import sendp
 
 def discover(interface: str):
     try:
         ip = f"{get_if_addr(interface)}/24"
         frame = ARP(pdst=ip) / Ether(dst="ff:ff:ff:ff:ff:ff")
-        srp(frame, timeout=1, verbose=False)
+        sendp(frame, iface=interface, verbose=False)
     except OSError:
         print(f"Could not open the adapter for interface {interface}")
     except Scapy_Exception:
